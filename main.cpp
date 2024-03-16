05897513ba61454fcaea1e996e61b4538e30d40a
sc30714 <vikram.vasudevan@comcast.net>
2024-03-16 12:25:32 -0700

    Finished the basics, now just need to pull it together for the final product

--- main.cpp
+++ main.cpp
@@ -11,37 +11,59 @@ bool search(Node* current, int value);
 void remove(Node* &root, Node* current, int value);
 
 int main(){
-    char input[50];
-    char method;
-    cout << "Would you like to enter by file (f) or by console (c)? " << endl;
-    cin >> method;
-    cin.get();
     Node* root = NULL;
-    if(method == 'c'){
-        cout << "How many numbers will you be entering? " << endl;
-        int numNums;
-        cin >> numNums;
+
+    bool stillRunning = true;
+    while(stillRunning = true){
+        cout << "What would you like to do? ADD, REMOVE, SEARCH, PRINT, or QUIT " << endl;
+        char choice[50];
+        cin.get(choice, 50);
         cin.get();
-        cout << "Enter your string of numbers: " << endl;
-        for(int i = 0; i < numNums; i++){
-            int tempInt;
-            cout << "INSIDE?" << endl;
-            cin >> tempInt;
-            cout << tempInt << endl;
-            cout << "HI" << endl;
+        if(strcmp(choice, "ADD") == 0){
+            char input[50];
+            char method;
+            cout << "Would you like to enter by file (f) or by console (c)? " << endl;
+            cin >> method;
             cin.get();
-            Node* newNode = new Node();
-            newNode->setInformation(tempInt);
-            add(root, root, newNode);
-        }  
-        print(root, 0); 
-        if(search(root, 4) == true){
-            cout << "That value exists!" << endl;
+            if(method == 'c'){
+                cout << "How many numbers will you be entering? " << endl;
+                int numNums;
+                cin >> numNums;
+                cin.get();
+                cout << "Enter your string of numbers: " << endl;
+                for(int i = 0; i < numNums; i++){
+                    int tempInt;
+                    cout << "INSIDE?" << endl;
+                    cin >> tempInt;
+                    cout << tempInt << endl;
+                    cout << "HI" << endl;
+                    cin.get();
+                    Node* newNode = new Node();
+                    newNode->setInformation(tempInt);
+                    add(root, root, newNode);
+                }  
+            
+            }
+            else if(method == 'f'){
+                
+            }
         }
-        else{
-            cout << "That value doesn't exist" << endl;
+        else if(strcmp(choice, "REMOVE") == 0){
+
         }
+        else if(strcmp(choice, "SEARCH") == 0){
+
+
+        }
+        else if(strcmp(choice, "PRINT") == 0){
+
+        }
+        else if(strcmp(choice, "QUIT") == 0){
+
+        }
+
     }
+    
 }
 
 bool search(Node* current, int value){
@@ -60,9 +82,35 @@ bool search(Node* current, int value){
     }
     return false;
 }
-void remove(Node* &root, Node* current, int value){
-    if(root->getInformation() == value){
 
+void remove(Node* &root, Node* current, int value){
+    cout << "VALUE: " << value << endl;
+    if(root->getInformation() == value){
+        if(root->getRight() == NULL && root->getLeft() == NULL){
+            delete root;
+        }
+        else if(root->getRight() != NULL && root->getLeft() == NULL){
+            Node* tempNode = root->getRight();
+            delete root;
+            root = tempNode;
+        }
+        else if(root->getRight() == NULL && root->getLeft() != NULL){
+            Node* tempNode = root->getLeft();
+            delete root;
+            root = tempNode;
+        }
+        else if(root->getRight() != NULL && root->getLeft() != NULL){
+            Node* tempNode = root->getRight();
+            while(tempNode->getLeft() != NULL){
+                tempNode = tempNode->getLeft();
+            }
+            Node* tempNode2 = new Node();
+            tempNode2->setInformation(tempNode->getInformation());
+            remove(root, tempNode, tempNode->getInformation());
+            tempNode2->setLeft(root->getLeft());
+            tempNode2->setRight(root->getRight());
+            root = tempNode2;
+        }
     }
     else if(current->getInformation() > value){
         if(current->getLeft()->getInformation() != value){
@@ -73,23 +121,63 @@ void remove(Node* &root, Node* current, int value){
                 delete current->getLeft();
                 current->setLeft(NULL);
             }
-            else if(current->getLeft()->getLeft() != NULL){
+            else if(current->getLeft()->getLeft() != NULL && current->getLeft()->getRight() == NULL){
+                Node* tempNode = current->getLeft()->getLeft();
+                delete current->getLeft();
+                current->setLeft(tempNode);
+            }
+            else if(current->getLeft()->getLeft() == NULL && current->getLeft()->getRight()!= NULL){
+                Node* tempNode = current->getLeft()->getRight();
+                delete current->getLeft();
+                current->setLeft(tempNode);
+            }
+            else if(current->getLeft()->getLeft() != NULL && current->getLeft()->getRight() != NULL){
                 Node* tempNode = current->getLeft()->getLeft();
                 while(tempNode->getRight() != NULL){
                     tempNode = tempNode->getRight();
-                }
+                }                
                 Node* tempNode2 = new Node();
                 tempNode2->setInformation(tempNode->getInformation());
-                tempNode = NULL;
-                
-
+                remove(root, current, tempNode->getInformation());
+                tempNode2->setLeft(current->getLeft()->getLeft());
+                tempNode2->setRight(current->getLeft()->getRight());
+                current->setLeft(tempNode2);
             }
         }
     }
     else if(current->getInformation() < value){
-
+        if(current->getRight()->getInformation() != value){
+            remove(root, current->getRight(), value);
+        }
+        else{
+            if(current->getRight()->getLeft() == NULL && current->getRight()->getRight() == NULL){
+                delete current->getRight();
+                current->setRight(NULL);
+            }
+            else if(current->getRight()->getLeft() != NULL && current->getRight()->getRight() == NULL){
+                Node* tempNode = current->getRight()->getLeft();
+                delete current->getRight();
+                current->setRight(tempNode);
+            }
+            else if(current->getRight()->getLeft() == NULL && current->getRight()->getRight()!= NULL){
+                Node* tempNode = current->getRight()->getRight();
+                delete current->getRight();
+                current->setRight(tempNode);
+            }
+            else if(current->getRight()->getLeft() != NULL && current->getRight()->getRight() != NULL){
+                Node* tempNode = current->getRight()->getRight();
+                while(tempNode->getLeft() != NULL){
+                    tempNode = tempNode->getLeft();
+                }                
+                Node* tempNode2 = new Node();
+                tempNode2->setInformation(tempNode->getInformation());
+                remove(root, current, tempNode->getInformation());
+                tempNode2->setLeft(current->getRight()->getLeft());
+                tempNode2->setRight(current->getRight()->getRight());
+                current->setRight(tempNode2);
+            }
     }
-
+    }
 }
 
 void add(Node* &root, Node* current, Node* newNode){
@@ -126,7 +214,5 @@ void print(Node* current, int count){
     if(current->getRight() != NULL){
         print(current->getRight(), count + 1);
     }
-
-
 
 }
